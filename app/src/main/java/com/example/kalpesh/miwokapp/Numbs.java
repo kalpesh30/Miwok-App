1a121750a6fa481b1d7a268fd255d6353f93e8a8
Kalpesh <kalpeshgupta30@gmail.com>
2018-07-21 20:49:34 +0530

    Added Numbers array and Log messages

--- app/src/main/java/com/example/kalpesh/miwokapp/Numbs.java
+++ app/src/main/java/com/example/kalpesh/miwokapp/Numbs.java
@@ -2,6 +2,7 @@ package com.example.kalpesh.miwokapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Numbs extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class Numbs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // An array of Words
+
+        String words[] = new String[10] ;
+        words[0] = "one" ;
+        words[1] = "two" ;
+        words[2] = "three" ;
+        words[3] = "four" ;
+        words[4] = "five" ;
+        words[5] = "six" ;
+        words[6] = "seven" ;
+        words[7] = "eight" ;
+        words[8] = "nine" ;
+        words[9] = "ten" ;
+
+        Log.v("Number's Actvity","Word at index 0 : " + words[0]) ;
+        Log.v("Number's Activity","Word at index 1 : " + words[1]) ;
     }
 }
